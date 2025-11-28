e9a4a478d29030b35814cdde8d905615aa639eac
Collins Kibet <kollcibe05@gmail.com>
2025-11-28 10:26:27 +0300

    ORM init

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,33 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()
+Department.create_table()
+
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)
+
+# payroll.save()
+# print(payroll)
+
+hr = Department.create("Human Resources", "Building c, East Wing")
+print(hr)
+
+
+quality_assurance = Department.create("Quality Asurance", 'Gaza Plaza')   
+
+quality_assurance.name = "Quality Assurance"
+quality_assurance.update()
+print(quality_assurance)
+
+
+
+staff_recruiters = Department.create("Staff recruitment", "Paza Plaza, 2nd Floor")
+print(staff_recruiters)
+
+staff_recruiters.delete()
+print(staff_recruiters)
+
+# hr.save()
+# print(hr)
 ipdb.set_trace()
